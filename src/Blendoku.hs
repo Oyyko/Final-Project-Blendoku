172e57e5f8713e519167a6b59dfaa3d0b68ce1ee
Xiaoyi Bao <xiaoyibao@Xiaoyis-MacBook-Pro.local>
2023-11-22 10:28:46 -0800

    fix bug that program exit when cursor is out of board

--- src/Blendoku.hs
+++ src/Blendoku.hs
@@ -11,7 +11,7 @@ module Blendoku
     , ColorVector
     , Coord
     , Direction(..)
-    , BlendokuGame 
+    , BlendokuGame
     , level
     , board, gtBoard
     , cursorPos, chosenPos
@@ -29,11 +29,12 @@ where
 
 import Control.Lens hiding (preview, op, zoom, (<|), chosen)
 import Data.Map (Map)
-import Data.Map as M (fromList, toList, (!), insert)
+import Data.Map as M (fromList, toList, (!), insert, member)
 import Linear.V2 (V2(..))
 import Data.String (String)
 import Control.Monad.Trans.State (StateT(..), gets, evalStateT, execStateT, modify, execStateT)
 import Control.Applicative (Applicative(pure))
+import Control.Monad (when)
 import System.Random (Random(..), newStdGen, mkStdGen)
 import Data.List (sortOn)
 
@@ -111,7 +112,7 @@ shift dir = do
       cell2 = board M.! cursor'
       cell2' = cell2 & hovered .~ True
       board' = M.insert cursor cell1' (M.insert cursor' cell2' board)
-  modify $ \g -> g { _board = board', _cursorPos = cursor'}
+  Control.Monad.when (M.member cursor' board) $ modify $ \g -> g { _board = board', _cursorPos = cursor'}
 
 updateCursor :: Coord -> Direction -> Coord
 updateCursor (V2 x y) dir = case dir of
@@ -127,16 +128,14 @@ swapWithChosen = do
   board <- gets _board
   let isNotChosen = (chosenPos == V2 0 0)
   let isSame = (chosenPos == cursorPos)
-  if isNotChosen || isSame then pure () 
+  if isNotChosen || isSame then pure ()
   else do
     let cell1 = board M.! chosenPos
         cell1' = cell1 & chosen .~ False & hovered .~ True
         cell2 = board M.! cursorPos
         cell2' = cell2 & chosen .~ True & hovered .~ False
-        -- cell2' = cell2 & chosen .~ False & hovered .~ False
         board' = M.insert chosenPos cell2' (M.insert cursorPos cell1' board)
     modify $ \g -> g { _board = board'}
-    -- modify $ \g -> g { _board = board', _chosenPos = V2 0 0}
 
 -- TODO: implementation for RGBcolor rather than gray
 colorToName :: ColorVector -> String
@@ -154,14 +153,14 @@ initGame = do
       , _gtBoard      = modifyFirstCell (generateBoard False candidateRows candidateCols)
       , _cursorPos       = V2 1 1
       -- V2 0, 0 means no chosen grid
-      , _chosenPos       = V2 0 0  
+      , _chosenPos       = V2 0 0
     }
 
 
 generateBoard :: Bool -> Int -> Int -> Board
-generateBoard False row col = M.fromList $ zip 
+generateBoard False row col = M.fromList $ zip
    (generateCoords row col) (generateGradientCells 0 255 (row * col))
-generateBoard True row col = M.fromList $ zip 
+generateBoard True row col = M.fromList $ zip
    (generateCoords row col) (shuffle (generateGradientCells 0 255 (row * col)))
 
 modifyFirstCell :: Board -> Board
