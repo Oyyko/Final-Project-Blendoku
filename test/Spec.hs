565fd265b4ba2e0cd1a9dadd4db165ebf8f65d14
Xiaoyi Bao <xiaoyibao@Xiaoyis-MacBook-Pro.local>
2023-12-09 09:58:27 -0800

    sample code for unit test

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,38 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE OverloadedStrings   #-}
+
+import Blendoku 
+import Test.Tasty
+import Test.Tasty.HUnit
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = do
+    defaultMain testSuite
+
+testSuite :: TestTree
+testSuite = testGroup "Blendoku Test Suite"
+    [ blendokuTests
+    , anotherTestGroup
+    ]
+
+blendokuTests :: TestTree
+blendokuTests = testGroup "Blendoku Tests"
+    [ testcase_1
+    ]
+
+testcase_1 :: TestTree
+testcase_1 = testCase "Example Test 1" $ do
+    assertEqual "Failed test case 1" True True
+
+anotherTestGroup :: TestTree
+anotherTestGroup = testGroup "Another Test Group"
+    [ testcase_2
+    ]
+
+testcase_2 :: TestTree
+testcase_2 = testCase "Example Test 2" $ do
+    assertEqual "Failed test case 2" True True
+
+
+
+
